489107c044b2ff3c931a632fb4ba93bc61292371
qujq <qujq@trip.com>
2024-08-26 21:28:17 +0800

    部分预渲染

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+const nextConfig = {
+  /* The experimental.ppr preview feature can only be enabled when using the latest canary version of Next.js.
+  See more info here: https://nextjs.org/docs/messages/ppr-preview */
+  // experimental: {
+  //   ppr: 'incremental',
+  // },
+};
 
 export default nextConfig;
